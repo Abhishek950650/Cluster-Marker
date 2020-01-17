c0aba7a25fbe123906c5521b4c810204e3a8d282
Abhishek950650 <Abhishek950650@gmail.com>
2020-01-17 15:41:30 +0530

    make dynamic upload data

--- clusters.js
+++ clusters.js
@@ -1,25 +1,72 @@
 
-var leaflet_tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
+var leaflet_tiles = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png'),
+    CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png');
 
 var map = L.map('map', {
-    center: [25.2744, 133.7751],
-    zoom: 4,
-    layers: [leaflet_tiles]
+    center: [28.0820, 77.6753],
+    zoom: 5,
+    layers: [leaflet_tiles, CartoDB_DarkMatter] //,CartoDB_DarkMatter add to load this tile
 });
 
-var mcg = L.markerClusterGroup({
-    chunkedLoading: true,
-    //singleMarkerMode: true,
-    spiderfyOnMaxZoom: false
+var baseMaps = {
+    "CartoDB_DarkMatter": CartoDB_DarkMatter,
+    "Leaflet_Tiles": leaflet_tiles
+};
+
+L.control.layers(baseMaps).addTo(map);
+
+
+document.getElementById("fileInput").addEventListener("change", function (evt) {
+    var file = evt.target.files[0], // Read only first file.
+        reader = new FileReader();
+
+    reader.onload = function (e) {
+        var fileText = e.target.result;
+        fileData = JSON.parse(fileText);
+
+
+        myIcon = L.icon({
+            iconUrl: 'marker-icon.png',
+            iconSize: [38, 95],
+            iconAnchor: [22, 94],
+            popupAnchor: [-3, -76],
+            shadowSize: [68, 95],
+            shadowAnchor: [22, 94]
+        });
+
+        // var markers = L.markerClusterGroup();
+        //dynamically change the markers behaviours
+        var markers = L.markerClusterGroup({
+            chunkedLoading: true,
+            showCoverageOnHover: false,
+            zoomToBoundsOnClick: true,
+            singleMarkerMode: false,
+            spiderfyShapePositions: function (count, centerPt) {
+                var distanceFromCenter = 35,
+                    markerDistance = 45,
+                    lineLength = markerDistance * (count - 1),
+                    lineStart = centerPt.y - lineLength / 2,
+                    res = [],
+                    i;
+
+                res.length = count;
+
+                for (i = count - 1; i >= 0; i--) {
+                    res[i] = new Point(centerPt.x + distanceFromCenter, lineStart + markerDistance * i);
+                }
+
+                return res;
+            }
+
+        });
+
+        mymap = L.geoJson(fileData, {
+            pointToLayer: function (feature, latlng) {
+                return L.marker(latlng, { icon: myIcon });
+            }
+        })
+        markers.addLayer(mymap);
+        markers.addTo(map);
+    }
+    reader.readAsText(file);
 });
-
-for (var i = 0; i < addressPoints.length; i++) {
-    var a = addressPoints[i];
-    // console.log(a);
-    var title = a[2];
-    var marker = L.marker(new L.LatLng(a[0], a[1]), { title: title });
-    marker.bindPopup(title);
-    mcg.addLayer(marker);
-}
-
-map.addLayer(mcg);
